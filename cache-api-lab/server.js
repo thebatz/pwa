10fd4689856dbae1b7627ea92f5caa8ba5b2a00e
Baptiste Foissac <baptiste.foissac@akqa.com>
2019-03-12 14:45:33 +0100

    add proto

---
+++ cache-api-lab/server.js
@@ -0,0 +1,48 @@
+
+
+const express = require('express');
+const appexpress = express();
+const https = require('https');
+const fs = require('fs');
+// // This serves static files from the specified directory
+appexpress.use(express.static(__dirname));
+
+// // we will pass our 'app' to 'https' server
+https.createServer({
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+    passphrase: 'helloworldhelloworld'
+}, appexpress)
+.listen(8081);
+
+
+
+
+// const express = require('express');
+// const app = express();
+
+// // This serves static files from the specified directory
+// app.use(express.static(__dirname));
+
+// const server = app.listen(8081, () => {
+
+//   const host = server.address().address;
+//   const port = server.address().port;
+
+//   console.log('App listening at http://%s:%s', host, port);
+// });
+
+
+
+
+// const fs = require('fs');
+
+// const options = {
+//   key: fs.readFileSync(__dirname + '/localhost-key.pem'),
+//   cert: fs.readFileSync(__dirname + '/localhost.pem')
+// };
+
+// require('https').createServer(options, (req, res) => {
+//   res.writeHead(200)
+//   res.end(`Got SSL?`)
+// }).listen(443);
