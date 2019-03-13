7c56f20616c00ab4e274787bfd4194becffcdbc1
Baptiste Foissac <baptiste.foissac@akqa.com>
2019-03-13 03:09:10 +0100

    clean

--- cache-api-lab/sw.js
+++ cache-api-lab/sw.js
@@ -1,19 +1,13 @@
 const filesToCache = [
   '/',
-  '/style/main.css',
-  '/images/still_life_medium.jpg',
-  '/images/images/volt_medium.jpg',
-  '/index.html',
-  '/pages/offline.html',
-  '/pages/404.html',
-  '/pages/post2.html',
-  './style/main.css',
-  './images/still_life_medium.jpg',
-  './images/images/volt_medium.jpg',
-  '//index.html',
-  './pages/offline.html',
-  './pages/404.html',
-  './pages/post2.html'
+  'style/main.css',
+  'images/still_life_medium.jpg',
+  'images/volt_medium.jpg',
+  'images/horses_medium.jpg',
+  'index.html',
+  'pages/offline.html',
+  'pages/post2.html',
+  'pages/404.html'
 ];
 
 const staticCacheName = 'pages-cache-v3';
@@ -45,19 +39,31 @@ self.addEventListener('activate', event => {
     })
   );
 });
+// self.addEventListener('fetch', function(event) {
+//   console.log('fetch');
+//   event.respondWith(
+//     fetch(event.request).catch(function() {
+//       return caches.match(event.request);
+//     })
+//   );
+// });
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch event for ', event.request.url);
+  // console.log('Fetch event for ', event.request.url);
   event.respondWith(
-    caches.match(event.request)
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+    // caches.match(event.request)
     .then(response => {
-      if (response) {
-        console.log('Found ', event.request.url, ' in cache');
-        return response;
-      }
-      console.log('Network request for ', event.request.url);
-      return fetch(event.request)
-      .then(response => {
+      // if (response) {
+      //   // console.log('Found ', event.request.url, ' in cache');
+      //   return response;
+      // }
+      // // console.log('Network request for ', event.request.url);
+
+      // return fetch(event.request)
+      // .then(response => {
         if (response.status === 404) {
           return caches.match('pages/404.html');
         }
@@ -65,7 +71,7 @@ self.addEventListener('fetch', event => {
         .then(cache => {
           cache.put(event.request.url, response.clone());
           return response;
-        });
+        // });
       });
     }).catch(error => {
       console.log('Error, ', error);
--- proto_manifest/sw.js
+++ proto_manifest/sw.js
@@ -3,3 +3,7 @@ importScripts("sw-toolbox.js");
 toolbox.precache(["index.html", "assets/css/main.css"]);
 toolbox.router.get("/images/*", toolbox.cacheFirst);
 toolbox.router.get("/*", toolbox.networkFirst, { networkTimeoutSeconds: 5 });
+
+if (window.matchMedia('(display-mode: standalone)').matches) {
+    console.log("This is running as standalone.");
+  }
